2c1159e3ce7cf4bd67e6a98e5a511d819e08c51c
vicen <xqfxqf111@gmail.com>
2025-11-10 18:34:45 +0800

    ch1: exercise1-31 done

--- chapter1/exercise1-31.rkt
+++ chapter1/exercise1-31.rkt
@@ -18,22 +18,37 @@
 
 (define (cube x) (* x x x))
 
+(define (identity x) x )
+
 (define (product-cubes-recursion a b)
     (product-recursion cube a inc b))
 (define (product-cubes-iteration a b)
     (product-iteration cube a inc b))
+
 (product-cubes-recursion 1 3)
 (product-cubes-iteration 1 3)
-
-(define (identity x) x )
 
 (define (factorial-recursion a b)
     (product-recursion identity a inc b))
 (define (factorial-iteration a b)
     (product-iteration identity a inc b))
+
 (factorial-recursion 1 5)
-(factorial-iteration 1 5)
 (factorial-recursion 1 10)
+(factorial-iteration 1 5)
 (factorial-iteration 1 10)
 
 ; 2/3 * 4/3 * 4/5 * 6/5 * 6/7
+; 2*4/3*3 * 4*6/5*5
+(define (compute-pi a b)
+    (define (term x)
+        (/  (* x (+ x 2)) 
+            (* (+ x 1) (+ x 1))))
+    (define (next x)
+        (+ x 2))
+    (* 4.0 (product-recursion term a next b)))
+
+(compute-pi 2 10)
+(compute-pi 2 100)
+(compute-pi 2 1000)
+(compute-pi 2 10000)
